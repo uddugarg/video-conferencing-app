10508b3618433c5a814e856e189aed093a15f2db
unknown <udivyy.99@gmail.com>
2021-04-28 00:15:29 +0530

    comments added

--- public/script.js
+++ public/script.js
@@ -1,32 +1,32 @@
-const socket = io('/');
+const socket = io('/');                     //A new Socket instance is returned for the namespace specified by the pathname in the URL, defaulting to /. 
 
-const videoGrid = document.getElementById('video-grid');
+const videoGrid = document.getElementById('video-grid');        //getting html element using id
 
-const myPeer = new Peer(undefined, {
+const myPeer = new Peer(undefined, {                    //Creating a new peer connection at port 3001 as it is locally running on the same port
     host: '/',
     port: '3001'
 })
 
-const myVideo = document.createElement('video');
+const myVideo = document.createElement('video');        //creating a new video element for adding the stream of the user
 myVideo.muted = true;
 
-navigator.mediaDevices.getUserMedia({
+navigator.mediaDevices.getUserMedia({                   // provides information about the web browser and its capabilities
     video: true,
     audio: true,
 }).then(stream => {
-    addVideoStream(myVideo, stream);
+    addVideoStream(myVideo, stream);                    //function which add video stream to the frontend
 
-    socket.on('user-connected', userId => {
-        connectToNewUser(userId, stream);
+    socket.on('user-connected', userId => {             //listening to the event when new user is connected to the room
+        connectToNewUser(userId, stream);               //function which connects new user to the room with its WebRTC connections
     })
 
-    myPeer.on('call', call => {
+    myPeer.on('call', call => {                         //listening to a peer connection that whenever call is answered stream is render to the room
         call.answer(stream);
     })
 })
 
-myPeer.on('open', id => {
-    socket.emit('join-room', ROOM_ID, id);
+myPeer.on('open', id => {                               //listening to a peer connection whenever someone joins the room
+    socket.emit('join-room', ROOM_ID, id);              //creating a event which return the userId with the roomId of the joined room
 })
 
 function connectToNewUser(userId, stream) {
--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const { v4: uuidV4 } = require('uuid');
+const app = express();                      //Creating or initializing a app using express
+const server = require('http').Server(app); //Creating a server using http module and initializing it to our App
+const io = require('socket.io')(server);    //passing server to the return of this require function
+const { v4: uuidV4 } = require('uuid');     //importing uuid module as uuidV4
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('view engine', 'ejs');              //It is a simple templating language/engine that lets its user generate HTML with plain javascript
+app.use(express.static('public'));          //serving the static files present in the public folder of the App
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {                                //route to create a new room and redirect the user to that room
     res.redirect(`${uuidV4()}`)
 });
 
-app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room });
+app.get('/:room', (req, res) => {                           //routes for rendering a room
+    res.render('room', { roomId: req.params.room });        //rendering a engine stored in a variable 'room' and passing a parameter roomId from Parameterized URL
 })
 
-io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.to(roomId).emit('user-connected', userId)
+io.on('connection', socket => {                             //Creating a connection using socket.io
+    socket.on('join-room', (roomId, userId) => {            //setting up an event which happens to run whenever someone enters the room
+        socket.join(roomId);                                //send roomId to client side
+        socket.to(roomId).emit('user-connected', userId)    //event which sends a message to the room whenever a user join that same room
     })
 })
 
-server.listen(3000, () => {
+server.listen(3000, () => {                 //listen to a port 3000
     console.log("server's running");
 });
